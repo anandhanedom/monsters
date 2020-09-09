6d440d5a447d5a4d63fe9fac5511eb1c5ed7131b
Anandhan <anandhanedom@gmail.com>
2020-09-10 02:40:29 +0530

    Updates

---
+++ precache-manifest.3f84fc5db4539234c7c00c3ec670b55d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1c8d11ab14626d195c12b562db93b7e",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "a2c19246a6820cf39b40",
+    "url": "/monsters/static/css/main.ae1d45c3.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2c19246a6820cf39b40",
+    "url": "/monsters/static/js/main.3670f717.chunk.js"
+  },
+  {
+    "revision": "03919b83b878cef951f1",
+    "url": "/monsters/static/js/runtime-main.cc0b5fe4.js"
+  }
+]);
